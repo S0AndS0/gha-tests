8bc360bb225087274d3b13a8574565a44dd266ec
S0AndS0 <strangerthanbland@gmail.com>
2019-10-08 16:40:47 -0700

    Tests _hunch_ about scoping and multi-Action chaining

--- .github/actions/docker-node/main.js
+++ .github/actions/docker-node/main.js
@@ -9,3 +9,15 @@ console.log('------');
 console.log('--- process.argv ---');
 console.table(process.argv)
 console.log('------');
+
+
+console.log(`process.env.INPUT_SASS_SOURCE -> ${process.env.INPUT_SASS_SOURCE}`);
+console.log(`process.env.INPUT_SASS_DESTINATION -> ${process.env.INPUT_SASS_DESTINATION}`);
+
+const sass_source = process.env.INPUT_SASS_SOURCE;
+const sass_destination = process.env.INPUT_SASS_DESTINATION;
+
+console.log(`sass_source -> ${sass_source}`);
+console.log(`sass_destination -> ${sass_destination}`);
+
+const sass = require('sass');
